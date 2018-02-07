2ea0919c64e16e3a054d81d7c7e12ca05deb4481
carlostrapet <trapet_llamas@hotmail.com>
2018-02-07 16:54:13 +0000

    replaced  references to

--- app.rb
+++ app.rb
@@ -11,9 +11,11 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-    $game = Game.new
+    # $player_1 = Player.new(params[:player_1_name])
+    # $player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(params[:player_1_name], params[:player_2_name])
+    @player_1 = $game.player_1
+    @player_2 = $game.player_2
     redirect('/play')
   end
 
@@ -21,8 +23,8 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
-  post '/attackp2' do
-    $game.attack($player_2)
+  post '/attack' do
+    $game.attack($game.player_2)
     erb(:attackconfirm)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new("Jimmy", "Steven") }
   let(:player1) { double :player }
   let(:player2) { double :player }
 
@@ -11,8 +11,11 @@ describe Game do
     it "reduces opponents hit points" do
       expect(player2).to receive(:reduce_hp)
       game.attack(player2)
-      # expect{player1.attack(player2)}.to change{player2.hit_points}.by(-10)
     end
-
+  end
+  context '#initialize' do 
+    it "initializes with a player class as argument" do 
+      expect(game.player_1).to be_an_instance_of Player
+    end
   end
 end
